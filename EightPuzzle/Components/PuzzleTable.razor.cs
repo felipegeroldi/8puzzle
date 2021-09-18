956fd237a93749c685e67a01a8a62750f636ca29
Felipe Geroldi <Felipe Geroldi>
2021-09-18 01:51:13 -0300

    Add Shuffle PuzzleTable Button

--- EightPuzzle/Components/PuzzleTable.razor.cs
+++ EightPuzzle/Components/PuzzleTable.razor.cs
@@ -11,7 +11,7 @@ namespace EightPuzzle.Components
         [Parameter]
         public bool AllowShuffle { get; set; } = false;
         private int CurrentIndex;
-        private int RowNumber;
+        private int CurrentRow;
         private List<List<Tile>> Rows;
 
         public PuzzleTable()
@@ -41,53 +41,107 @@ namespace EightPuzzle.Components
 
         private void ResetSelectedValues()
         {
-            RowNumber = 0;
+            CurrentRow = 0;
             CurrentIndex = -1;
         }
 
         private void ShuffleTiles()
         {
-            for(int i=0; i<100; i++)
+            Random rng = new Random();
+            int i, XSortedDirection, YSortedDirection, newRow, newIndex, currentRow, currentIndex;
+
+            for(i=0; i<100; i++)
             {
-                for (; CurrentIndex < 0 && RowNumber < Rows.Count; RowNumber++)
+                currentRow = 0;
+                currentIndex = -1;
+
+
+                for (; currentIndex < 0 && currentRow < Rows.Count; currentRow++)
                 {
-                    var row = Rows.ElementAt(RowNumber);
-                    CurrentIndex = row.FindIndex(t => t.Value == 0);
+                    var row = Rows.ElementAt(currentRow);
+                    currentIndex = row.FindIndex(t => t.Value == 0);
                 }
+                currentRow -= 1;
+
+                #region Finding new X Position
+                newIndex = currentIndex;
+
+                // <- Even | Odd ->
+                XSortedDirection = rng.Next(1, 10);
+                if ((XSortedDirection % 2) == 0 && currentIndex + 1 < Rows.ElementAt(currentRow).Count)
+                {
+                    newIndex += 1;
+                }
+                else if (currentIndex - 1 >= 0)
+                {
+                    newIndex -= 1;
+                }
+                #endregion
+
+                #region Finding new Y Position
+                newRow = currentRow;
+
+                // /\ Even | Odd \/
+                YSortedDirection = rng.Next(1, 10);
+                if ((YSortedDirection % 2) == 0 && currentRow + 1 < Rows.Count)
+                {
+                    newRow += 1;
+                } else if (currentRow - 1 >= 0) {
+                    newRow -= 1;
+                }
+                #endregion
+
+                var currentTile = Rows.ElementAt(currentRow)
+                    .ElementAt(currentIndex);
+
+                var destinationTile = Rows.ElementAt(newRow)
+                    .ElementAt(newIndex);
+
+                Rows[newRow][newIndex] = currentTile;
+                Rows[currentRow][currentIndex] = destinationTile;
+
+                foreach (var row in Rows)
+                {
+                    foreach (var tile in row)
+                    {
+                        Console.Write($"{tile.Value} ");
+                    }
+                    Console.Write("\n");
+                }
+
+                Console.WriteLine("-------");
             }
+
+            StateHasChanged();
         }
 
         private void StartDrag(Tile tile)
         {
             ResetSelectedValues();
 
-            for (; CurrentIndex < 0 && RowNumber < Rows.Count; RowNumber++)
+            for (; CurrentIndex < 0 && CurrentRow < Rows.Count; CurrentRow++)
             {
-                var row = Rows.ElementAt(RowNumber);
+                var row = Rows.ElementAt(CurrentRow);
                 CurrentIndex = row.FindIndex(t => t.Value == tile.Value);
             }
         }
 
         void Drop(Tile destinationTile)
         {
-            if (destinationTile is not null && RowNumber > 0)
+            if (destinationTile is not null && CurrentRow > 0)
             {
-                var currentTile = Rows.ElementAt(RowNumber - 1)
+                int destinationIndex = -1, destinationRow = 0;
+                for (; destinationIndex < 0 && destinationRow < Rows.Count; destinationRow++)
+                {
+                    var row = Rows.ElementAt(destinationRow);
+                    destinationIndex = row.FindIndex(t => t.Value == destinationTile.Value);
+                }
+
+                var currentTile = Rows.ElementAt(CurrentRow - 1)
                     .ElementAt(CurrentIndex);
 
-                int currentTileValue = currentTile.Value;
-                string currentTileColor = currentTile.Color;
-
-
-                int destinationTileValue = destinationTile.Value;
-                string destinationTileColor = destinationTile.Color;
-
-
-                destinationTile.Value = currentTileValue;
-                destinationTile.Color = currentTileColor;
-
-                currentTile.Value = destinationTileValue;
-                currentTile.Color = destinationTileColor;
+                Rows[CurrentRow - 1][CurrentIndex] = destinationTile;
+                Rows[destinationRow - 1][destinationIndex] = currentTile;
 
                 ResetSelectedValues();
                 StateHasChanged();
